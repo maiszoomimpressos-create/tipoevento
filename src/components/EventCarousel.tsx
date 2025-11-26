aea03fdc3b71e8103f194e9cab8427f96dd1cfc6
[dyad] <lfcdigitaldv@gmail.com>
2025-11-25 21:19:14 -0300

    [dyad] Revertendo o carrossel - wrote 1 file(s)

--- src/components/EventCarousel.tsx
+++ src/components/EventCarousel.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useEmblaCarousel, { EmblaCarouselType } from 'embla-carousel-react';
